b4ff186c2565d34530633bbf6494423b2ad654e3
Adnan Shaldrake <ashaldrake@gmail.com>
2022-09-05 19:26:48 +0530

    added list

--- phonebook.sh
+++ phonebook.sh
@@ -13,12 +13,20 @@ add_item() {
     echo "$1 $2" >> $FILE 
 }
 
+list_phonebook(){
+    cat $FILE 
+}
+
 create_phonebook $FILE
 
 if [ $1 == "new" ]; 
 then
-    echo "new"
+    echo "Adding "$2" $3"
     add_item "$2" $3
+elif [ $1 == "list" ];
+then
+    echo "Listing phonebook items..."
+    list_phonebook
 else
     echo "other"
 fi
